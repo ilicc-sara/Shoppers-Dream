fdd75a1db90c1d2ea1336db1b1b5080e25624eda
Sara <sarailic431@gmail.com>
2025-08-27 20:56:47 +0200

    /.../

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,37 @@ import {
   benefits,
 } from "./homeComponents/helpers";
 
+const testimonials = [
+  {
+    header: "These pieces transformed my home!",
+    text: "The level of hospitality and professionalism shown by the company made my shopping experience a breeze. The modern designs and top-quality materials of these products exceeded my expectations, and I couldn't be happier with my purchase!",
+    name: "Anastasiya Petrova",
+    location: "Kyiv, Ukraine",
+    image: "./testimonialsPeople/human1.webp",
+  },
+  {
+    header: "My space now finally feels complete.",
+    text: "The beautiful furniture pieces I purchased fit perfectly in my home and bring me so much joy every day. The quality and modern design exceeded my expectations, and the entire experience with your company was seamless.",
+    name: "Sophie Martin",
+    location: "Paris, France",
+    image: "./testimonialsPeople/human2.jpg",
+  },
+  {
+    header: "You guys brought vision back to my life.",
+    text: "I cannot speak highly enough of the quality of the products I received from this company. The attention to detail and craftsmanship is unparalleled. I am so grateful to have found such a reliable and professional source for my furniture needs.",
+    name: "Tyron Mayers",
+    location: "San Francisco, USA",
+    image: "./testimonialsPeople/human3.jpg",
+  },
+  {
+    header: "Can't believe I have been missing on this!",
+    text: "The furniture I found on this e-commerce store was exactly what I was looking for. The pieces have truly transformed my home and brought my vision to life, adding a touch of sophistication and elegance to every corner.",
+    name: "Vinney Malesh",
+    location: "Los Angeles, USA",
+    image: "./testimonialsPeople/human4.jpg",
+  },
+];
+
 function App() {
   const [benefit, setBenefit] = useState(benefits[0]);
 
@@ -120,10 +151,6 @@ function App() {
           </p>
           <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
             Where simplicity meets efficiency to bring your home to life.
-            <i class="bxr  bx-handshake"></i>
-            <i class="bxr  bx-check"></i>
-            <i class="bxr  bx-dollar-circle"></i>
-            <i class="bxr  bx-bolt"></i>
           </h1>
 
           <div className="!mx-auto w-[95%] flex justify-between items-center !my-[5rem]">
@@ -145,6 +172,29 @@ function App() {
             benefit={benefit.offer}
             text={benefit.offerText}
           />
+        </div>
+      </section>
+
+      <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
+        <div className="max-w-[90rem] !mx-auto">
+          <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
+            Testimonials
+          </p>
+          <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
+            Discover the stories of our delighted customers - thousands and
+            counting!
+          </h1>
+
+          <div>
+            <p>{testimonials[0].header}</p>
+            <p>{testimonials[0].text}</p>
+            <img
+              className="w-[5rem] h-[5rem] rounded-[222px] object-cover"
+              src={`${testimonials[0].image}`}
+            />
+            <p>{testimonials[0].name}</p>
+            <p>{testimonials[0].location}</p>
+          </div>
         </div>
       </section>
     </>
