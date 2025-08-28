196f25129bc5fccd93c69616286e50727a9075de
Sara <sarailic431@gmail.com>
2025-08-28 19:12:40 +0200

    scroll reveal sections

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,6 +12,7 @@ import {
   benefits,
   testimonials,
 } from "./homeComponents/helpers";
+import Reveal from "./homeComponents/Reveal";
 
 function Home() {
   const [time, setTime] = useState(5);
@@ -74,117 +75,126 @@ function Home() {
         </div>
       </footer>
 
-      <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
-        <div className="max-w-[120rem] !mx-auto">
-          <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium ">
-            Featured products
-          </p>
-          <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
-            The art of modern living unlocked.
-          </h1>
+      <Reveal>
+        <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
+          <div className="max-w-[120rem] !mx-auto">
+            <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium ">
+              Featured products
+            </p>
+            <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
+              The art of modern living unlocked.
+            </h1>
 
-          <div className="flex justify-between !pt-[7rem] !pb-[4rem]">
-            {exampleProductsImg.map((img, index) => (
-              <ExampleProducts key={index} image={img} />
-            ))}
+            <div className="flex justify-between !pt-[7rem] !pb-[4rem]">
+              {exampleProductsImg.map((img, index) => (
+                <ExampleProducts key={index} image={img} />
+              ))}
+            </div>
+            <Button variation="primary">All Products</Button>
           </div>
-          <Button variation="primary">All Products</Button>
-        </div>
-      </section>
+        </section>
+      </Reveal>
 
-      <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
-        <div className="max-w-[120rem] !mx-auto">
-          <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
-            Creeds we live by
-          </p>
-          <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
-            Elevate your home with our attitude of excellence and timeless
-            style.
-          </h1>
+      <Reveal>
+        <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
+          <div className="max-w-[120rem] !mx-auto">
+            <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
+              Creeds we live by
+            </p>
+            <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
+              Elevate your home with our attitude of excellence and timeless
+              style.
+            </h1>
 
-          <div className="flex justify-between !mt-[6rem] !mb-[3rem]">
-            {pillars.map((pillar, index) => (
-              <CoreValues
-                key={index}
-                icon={pillar.iconName}
-                value={pillar.coreValue}
-                text={pillar.description}
-              />
-            ))}
+            <div className="flex justify-between !mt-[6rem] !mb-[3rem]">
+              {pillars.map((pillar, index) => (
+                <CoreValues
+                  key={index}
+                  icon={pillar.iconName}
+                  value={pillar.coreValue}
+                  text={pillar.description}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Reveal>
 
-      <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
-        <div className="max-w-[90rem] !mx-auto">
-          <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
-            Operations
-          </p>
-          <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
-            Where simplicity meets efficiency to bring your home to life.
-          </h1>
+      <Reveal>
+        <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
+          <div className="max-w-[90rem] !mx-auto">
+            <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
+              Operations
+            </p>
+            <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
+              Where simplicity meets efficiency to bring your home to life.
+            </h1>
 
-          <div className="!mx-auto w-[95%] flex justify-between items-center !my-[5rem]">
-            {benefits.map((offer, index) => (
-              <Button
-                key={index}
-                variation={`${
-                  benefit.button === offer.button ? "primary" : "secundary"
-                }`}
-                handleClick={() => setBenefit(benefits[offer.id])}
-              >
-                {offer.button}
-              </Button>
-            ))}
-          </div>
-
-          <Benefits
-            icon={benefit.offerIcon}
-            benefit={benefit.offer}
-            text={benefit.offerText}
-          />
-        </div>
-      </section>
-
-      <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
-        <div className="max-w-[90rem] !mx-auto">
-          <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
-            Testimonials
-          </p>
-          <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
-            Discover the stories of our delighted customers - thousands and
-            counting!
-          </h1>
-
-          <div className="flex max-w-[70rem] !mx-auto items-center">
-            <Button variation="arrow" handleClick={() => {}}>
-              <i class="bxr  bx-arrow-left-stroke"></i>
-            </Button>
-            <div className="flex flex-col items-start max-w-[58rem] !mx-auto gap-[1.5rem] !my-[6rem]">
-              <p className=" text-left text-[2.8rem] leading-[1.9rem] font-semibold">
-                {testimonials[0].header}
-              </p>
-              <div className={`bg-[#d946ef] w-[${width}%] h-[0.5rem]`}></div>
-
-              <p className="text-left text-[1.5rem] leading-[2.8rem]">
-                {testimonials[0].text}
-              </p>
-              <img
-                className="w-[5rem] h-[5rem] rounded-[222px] object-cover"
-                src={`${testimonials[0].image}`}
-              />
-              <p className="text-[1.4rem] font-medium">
-                {testimonials[0].name}
-              </p>
-              <p className="text-[1.2rem]">{testimonials[0].location}</p>
+            <div className="!mx-auto w-[95%] flex justify-between items-center !my-[5rem]">
+              {benefits.map((offer, index) => (
+                <Button
+                  key={index}
+                  variation={`${
+                    benefit.button === offer.button ? "primary" : "secundary"
+                  }`}
+                  handleClick={() => setBenefit(benefits[offer.id])}
+                >
+                  {offer.button}
+                </Button>
+              ))}
             </div>
 
-            <Button variation="arrow" handleClick={() => {}}>
-              <i class="bxr  bx-arrow-right-stroke"></i>
-            </Button>
+            <Benefits
+              icon={benefit.offerIcon}
+              benefit={benefit.offer}
+              text={benefit.offerText}
+            />
           </div>
-        </div>
-      </section>
+        </section>
+      </Reveal>
+
+      <Reveal>
+        <section className="border-b-[2px] border-b-[#d5d5d5] !py-[14rem]">
+          <div className="max-w-[90rem] !mx-auto">
+            <p className="text-left uppercase text-[1.4rem] text-[#d946ef] font-medium">
+              Testimonials
+            </p>
+            <h1 className="text-[3.7rem] font-medium text-left text-[#444]">
+              Discover the stories of our delighted customers - thousands and
+              counting!
+            </h1>
+
+            <div className="flex max-w-[70rem] !mx-auto items-center">
+              <Button variation="arrow" handleClick={() => {}}>
+                <i class="bxr  bx-arrow-left-stroke"></i>
+              </Button>
+              <div className="flex flex-col items-start max-w-[58rem] !mx-auto gap-[1.5rem] !my-[6rem]">
+                <p className=" text-left text-[2.8rem] leading-[1.9rem] font-semibold">
+                  {testimonials[0].header}
+                </p>
+                <div
+                  className={`bg-[#d946ef] w-[${String(width)}%] h-[0.5rem]`}
+                ></div>
+
+                <p className="text-left text-[1.5rem] leading-[2.8rem]">
+                  {testimonials[0].text}
+                </p>
+                <img
+                  className="w-[5rem] h-[5rem] rounded-[222px] object-cover"
+                  src={`${testimonials[0].image}`}
+                />
+                <p className="text-[1.4rem] font-medium">
+                  {testimonials[0].name}
+                </p>
+                <p className="text-[1.2rem]">{testimonials[0].location}</p>
+              </div>
+              <Button variation="arrow" handleClick={() => {}}>
+                <i class="bxr  bx-arrow-right-stroke"></i>
+              </Button>
+            </div>
+          </div>
+        </section>
+      </Reveal>
     </>
   );
 }
---
+++ src/pages/home/homeComponents/Reveal.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+
+function Reveal(props) {
+  const { children, width = "100%" } = props;
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      console.log(isInView);
+    }
+  }, [isInView]);
+  return (
+    <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 1 }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+}
+
+export default Reveal;
