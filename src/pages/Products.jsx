562897dd56fed19de06f6c1986d482ca12294207
Sara <sarailic431@gmail.com>
2025-08-29 22:54:30 +0200

    ...... ....... ..... .... . ..

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function Products() {
-  return <h1>Products</h1>;
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(
+          "https://www.course-api.com/react-store-products"
+        );
+        const posts = await response.json();
+        setProducts(posts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPost();
+  }, []);
+  return (
+    <>
+      <h1>Products</h1>
+      <div className="container">
+        <ul className="furniture-list">
+          {products?.map((product, index) => (
+            <li key={index} className="furniture-item">
+              <img
+                src={product.image}
+                alt="Furniture-Picture"
+                className="furniture-picture"
+                style={{ display: "block" }}
+              />
+              <div className="furniture-item-info">
+                <h3 className="furniture-name">{product.name}</h3>
+                <h3 className="furniture-price">${+product.price / 100} $</h3>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Products;
