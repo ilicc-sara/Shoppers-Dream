eb00b2a004eb92c30a7cc54248c449337c5e8fa6
Sara <sarailic431@gmail.com>
2025-09-02 17:06:16 +0200

    ... ... ... .  . . . .... . . . .

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      DEFAULT: "1280px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
