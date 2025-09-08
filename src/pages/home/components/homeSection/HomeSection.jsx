8a5a5327245eda9d2a569242ab82a9a054fc74a4
Sara <sarailic431@gmail.com>
2025-09-08 18:40:23 +0200

    .......

--- src/pages/home/components/homeSection/HomeSection.jsx
+++ src/pages/home/components/homeSection/HomeSection.jsx
@@ -10,17 +10,17 @@ function HomeSection({ scrollToCoreValues, homeSectionRef }) {
       className="flex flex-col w-full border-b-[2px] border-b-[#d5d5d5]"
     >
       <section
-        className="grid tablet:grid-cols-2  justify-center place-items-center 
-      !mx-auto !my-[30px] desktop:w-6xl desktop:bg-green-300 laptop:w-5xl laptop:bg-blue-400 smallLT:w-4xl smallLT:bg-red-300 tablet:w-3xl tablet:bg-pink-400"
+        className="grid tablet:grid-cols-2 max-tablet:grid-rows-2 justify-center place-items-center 
+      !mx-auto !my-[30px] desktop:w-6xl  laptop:w-5xl  smallLT:w-4xl  tablet:w-3xl "
       >
-        <div className="flex flex-col gap-[20px] justify-start ">
-          <h1 className="font-bold text-[#404040] text-left  desktop:!text-[48px] desktop:text-red-600 desktop:!leading-[67px] laptop:!text-[40px] laptop:text-blue-500 laptop:!leading-[57px] smallLT:!text-[34px] smallLT:text-yellow-600 tablet:!text-[28px] tablet:!leading-[48px] ">
+        <div className="flex flex-col gap-[20px] tablet:justify-start max-tablet:justify-center">
+          <h1 className="font-bold text-[#404040] tablet:text-left max-tablet:text-center  desktop:!text-[48px] desktop:!leading-[67px] laptop:!text-[40px]  laptop:!leading-[57px] smallLT:!text-[34px] tablet:!text-[28px] tablet:!leading-[48px] max-mobile:!text-[34px] ">
             Furniture design to <br></br> make you feel at home
           </h1>
-          <p className=" tracking-[0.5px] font-medium text-left desktop:!text-[24px] desktop:!text-red-300 laptop:!text-[20px] laptop:text-green-300">
+          <p className=" tracking-[0.5px] font-medium tablet:text-left max-tablet:text-center desktop:!text-[24px]  laptop:!text-[20px] ">
             Create your perfect space with our designs.
           </p>
-          <div className="flex gap-[15px]">
+          <div className="flex gap-[15px] w-[100%] tablet:justify-start max-tablet:justify-center">
             <Link to="/products">
               <Button variation="primary">Explore products</Button>
             </Link>
@@ -32,7 +32,7 @@ function HomeSection({ scrollToCoreValues, homeSectionRef }) {
             </Button>
           </div>
         </div>
-        <div className="flex justify-end items-center w-[100%]">
+        <div className="flex tablet:justify-end max-tablet:justify-center items-center w-[100%]">
           <img
             className="w-[490px] h-[570px] max-smallLT:w-[400px] max-smallLT:h-[465px]"
             src="./heroImg.png"
@@ -41,8 +41,8 @@ function HomeSection({ scrollToCoreValues, homeSectionRef }) {
       </section>
 
       <footer className=" ">
-        <div className="!py-[120px] !mx-auto flex flex-col gap-[30px] max-w-6xl">
-          <p className="text-[14px] text-gray-500 uppercase font-semibold">
+        <div className="!py-[120px] !mx-auto flex flex-col gap-[30px] desktop:w-6xl  laptop:w-5xl  smallLT:w-4xl  tablet:w-3xl mobile:w-2xl smallmobile:w-[90%]">
+          <p className="tablet:text-[14px] max-tablet:text-[12px] text-gray-500 uppercase font-semibold">
             As featured in
           </p>
           <div className=" flex justify-between ">
--- src/pages/home/components/homeSection/components/FeaturesImg.jsx
+++ src/pages/home/components/homeSection/components/FeaturesImg.jsx
@@ -1,7 +1,7 @@
 function FeaturesImg({ image }) {
   return (
     <img
-      className="brightness-0 opacity-50 featured-img h-[30px]"
+      className="brightness-0 opacity-50 featured-img laptop:h-[30px] smallLT:h-[25px] tablet:h-[20px] max-tablet:h-[15px] "
       src={image}
     />
   );
